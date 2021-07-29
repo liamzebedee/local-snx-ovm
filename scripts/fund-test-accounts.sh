b3e6db236205bc24fadb848ddf0339f079f877b1
liamzebedee <liamzebedee@yahoo.com.au>
2021-07-29 16:21:31 +1000

    refactor: test balances script

--- scripts/fund-test-accounts.sh
+++ scripts/fund-test-accounts.sh
@@ -6,5 +6,5 @@ export HARDHAT_ACCOUNT_1_PRIVKEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5
 
 cd projects/synthetix/
 
-# Fund local account with SNX.
-node_modules/.bin/hardhat get-snx-local-l2 --account 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --provider http://localhost:8545 --snx-network local --private-key $HARDHAT_ACCOUNT_1_PRIVKEY
+# Fund local accounts with SNX.
+node_modules/.bin/hardhat fund-local-accounts --provider-url http://localhost:8545 --target-network local --private-key $HARDHAT_ACCOUNT_1_PRIVKEY
