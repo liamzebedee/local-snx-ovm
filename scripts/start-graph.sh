fd26fec1784394115772296cb9578b4ca477b443
liamzebedee <liamzebedee@yahoo.com.au>
2021-07-30 14:04:02 +1000

    fix: apostrophe in sed script

--- scripts/start-graph.sh
+++ scripts/start-graph.sh
@@ -3,7 +3,7 @@ cd projects/graph-node
 
 cd docker/
 # Replace "mainnet" with "optimism-local" network.
-sed -i -e "s/ethereum: 'mainnet/ethereum: optimism-local/" docker-compose.yml
+sed -i -e "s/ethereum: 'mainnet/ethereum: 'optimism-local/" docker-compose.yml
 
 # Start IPFS, Postgres, graph-node.
 docker-compose up -d
