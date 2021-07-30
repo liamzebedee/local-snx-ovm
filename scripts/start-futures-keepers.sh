d2a9b83548d1bca783597c54881e321714e2f2eb
liamzebedee <liamzebedee@yahoo.com.au>
2021-07-30 16:19:45 +1000

    fix futures keepers script

--- scripts/start-futures-keepers.sh
+++ scripts/start-futures-keepers.sh
@@ -10,4 +10,7 @@ if [ ! -f ./.env ]; then
     echo "ORACLE_PRIVATE_KEY=59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d" >> .env
 fi
 
-NETWORK=local node kovan.js
+# Account #1: 0x96D6C55a500782ba07aefb4f620dF2a94CDc7bA7
+export ETH_HDWALLET_MNEMONIC='height phrase soldier tiny stage suspect virtual power alert baby nothing borrow camera pulse agent'
+
+NETWORK=local node src/ run -p ws://localhost:8546 --from-block 0 -n 1
